aa6002f3c3d9ce8ae46da412f1ca8bea4ff66b2c
Ryan Dale <rd01748@georgiasouthern.edu>
2015-07-26 13:20:11 -0400

    Fix typo in Explosion

--- src/js/Explosion.js
+++ src/js/Explosion.js
@@ -4,7 +4,7 @@ import GameObject from 'GameObject';
 const explosionSprite = new GameImage('http://i.imgur.com/J7EHN8k.png').getImage();
 
 export default class Explosion extends GameObject {
-    constructor(x, yq) {
+    constructor(x, y) {
         super(x, y, explosionSprite);
     }
 }
