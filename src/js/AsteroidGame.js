dc0dabf35f58105be2dcbd209ddc9709b95110df
Ryan Dale <rd01748@georgiasouthern.edu>
2015-07-27 19:06:44 -0400

    Remove Cannon and Ship code from AsteroidGame.js

--- src/js/AsteroidGame.js
+++ src/js/AsteroidGame.js
@@ -8,21 +8,17 @@ import LifeManager from 'LifeManager';
 import ScoreManager from 'ScoreManager';
 import canvasManager from 'canvasManager';
 import Explosion from 'Explosion';
+import Ship from 'Ship';
 
 let backgroundTex = new GameImage('http://i.imgur.com/31uPjb0.png').getImage(),
-    shipTex = new GameImage('http://i.imgur.com/DxukQzY.png').getImage(),
-    cannonTex = new GameImage('http://i.imgur.com/VETLA6c.png').getImage(),
     primaryBackground = new GameImage('http://i.imgur.com/CVcWgas.png').getImage(),
     parallaxBackground = new GameImage('http://i.imgur.com/REMZBAf.png').getImage();
 
 const _DRAW = new WeakMap(),
     _UPDATE = new WeakMap(),
-    SHIP_Y = new WeakMap(),
-    CANNON_DELAY = new WeakMap(),
     GAME_OVER = new WeakMap();
 
-let shipHit = 20,
-    primaryScroll = 0,
+let primaryScroll = 0,
     primaryScroll2 = 1920,
     parallaxScroll = 0,
     parallaxScroll2 = 1680,
@@ -31,18 +27,16 @@ let shipHit = 20,
 export default class AsteroidGame {
     constructor() {
         this.asteroids = [];
-        this.cannons = [];
         this.canvas = canvasManager('canvas');
         this.context = this.canvas.getContext('2d');
         this.explosion = null;
+        console.log(this.canvas.height);
+        this.ship = new Ship(64, this.canvas.height / 2); //-height/2
 
         gameState.score = 0;
         gameState.lives = 3;
 
-        /* Initialize private variables */
         GAME_OVER.set(this, false);
-        SHIP_Y.set(this, 360 - shipTex.width / 2);
-        CANNON_DELAY.set(this, 0);
 
         this.context.fillStyle = "yellow";
         this.context.font = "bold 64px Arial";
@@ -51,27 +45,6 @@ export default class AsteroidGame {
         //_UPDATE.set(this, setInterval(this.update, 10));
         setInterval(this.draw.bind(this), 10);
         setInterval(this.update.bind(this), 10);
-
-        document.addEventListener('keydown', (event) => {
-            if (this.getGameOver()) {
-                return;
-            }
-            if ((event.keyCode == 40 || event.keyCode == 83) && SHIP_Y.get(this) < 536) {
-                SHIP_Y.set(this, SHIP_Y.get(this) + 32);
-            }
-            else if ((event.keyCode == 38 || event.keyCode == 87) && SHIP_Y.get(this) > 0) {
-                SHIP_Y.set(this, SHIP_Y.get(this) - 32);
-            } else if (event.keyCode == 27) {
-                gameState.paused = !gameState.paused;
-            }
-        });
-        document.addEventListener('keyup', (event) => {
-            if ((event.keyCode == 13 || event.keyCode == 32) && CANNON_DELAY.get(this) > 7 && !this.getGameOver()) {
-                CANNON_DELAY.set(this, 0);
-                let c = new Cannon(shipTex.width + 64, SHIP_Y.get(this) + shipTex.height / 2 - cannonTex.height / 2);
-                this.cannons.push(c);
-            }
-        });
 
         this.lifeManager = new LifeManager(3, 10, 10);
         this.scoreManger = new ScoreManager(0, this.canvas.width - 10, 60);
@@ -108,9 +81,10 @@ export default class AsteroidGame {
             this.explosion.draw(this.context);
         }
 
+        _.each(Cannon.getCannons(), (cannon) => cannon.draw(this.context));
         _.each(this.asteroids, (asteroid) => asteroid.draw(this.context));
-        _.each(this.cannons, (cannon) => cannon.draw(this.context));
-        this.context.drawImage(shipTex, 64, SHIP_Y.get(this));
+
+        this.ship.draw(this.context);
         if (GAME_OVER.get(this)) {
             let gameOverText = 'GAMEOVER',
                 formattedScore = `SCORE: ${gameState.score.toLocaleString()}`;
@@ -148,21 +122,20 @@ export default class AsteroidGame {
         if (parallaxScroll2 <= -1680) {
             parallaxScroll2 = parallaxScroll + 1680;
         }
-        CANNON_DELAY.set(this, CANNON_DELAY.get(this) + 1);
+
         _.each(this.asteroids, (asteroid) => {
             asteroid.update();
-            if (new Rectangle(64, SHIP_Y.get(this), shipTex.width * 3 / 4, shipTex.height).checkCollision(asteroid.rect)) {
+            if (this.ship.checkCollision(asteroid)) {
                 gameState.lives--;//LIVES.set(this, LIVES.get(this) - 1);// flash=true;//alert("GAME OVER");
                 this.lifeManager.removeLife();
                 asteroid.destroyAsteroid();
-                shipHit = 0;
-                this.explosion = new Explosion(64, SHIP_Y);
+                this.explosion = new Explosion(this.ship.x, this.ship.y);
                 setTimeout(()=> {
                     this.explosion = null;
                 }, 250)
             }
 
-            let cannon = _.find(this.cannons, (cannon) => asteroid.checkCollision(cannon));
+            let cannon = _.find(Cannon.getCannons(), (cannon) => asteroid.checkCollision(cannon));
 
             if (cannon) {
                 asteroid.destroyAsteroid();
@@ -173,10 +146,11 @@ export default class AsteroidGame {
             }
         });
 
-        _.each(this.cannons, (cannon) => cannon.update());
+        _.each(Cannon.getCannons(), (cannon) => cannon.update());
+        this.ship.update();
 
         if (this.explosion) {
-            this.explosion.setPosition(64, SHIP_Y.get(this));
+            this.explosion.setPosition(this.ship.x, this.ship.y);
         }
     }
 }
